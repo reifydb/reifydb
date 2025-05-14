7812c64cb499444dae0570c8e74288abe6d9957e
Dominique Chuo <dominique@reifydb.com>
2025-05-14 15:53:44 +0800

    support query and without parans

--- crates/rql/src/ast/parse/insert.rs
+++ crates/rql/src/ast/parse/insert.rs
@@ -2,9 +2,10 @@
 // This file is licensed under the AGPL-3.0-or-later
 
 use crate::ast::lex::Keyword::Into;
+use crate::ast::lex::Operator::OpenParen;
 use crate::ast::lex::{Keyword, Operator, Separator, TokenKind};
-use crate::ast::parse::Parser;
-use crate::ast::{AstInsert, parse};
+use crate::ast::parse::{Parser, Precedence};
+use crate::ast::{AstInsert, AstTuple, parse};
 
 impl Parser {
     pub(crate) fn parse_insert(&mut self) -> parse::Result<AstInsert> {
@@ -12,13 +13,26 @@ impl Parser {
 
         let mut rows = Vec::new();
 
-        loop {
-            let tuple = self.parse_tuple()?;
-            rows.push(tuple);
-            if self.consume_if(TokenKind::Keyword(Into))?.is_some() {
-                break;
+        if self.current()?.kind == TokenKind::Operator(OpenParen) {
+            loop {
+                let tuple = self.parse_tuple()?;
+                rows.push(tuple);
+                if self.consume_if(TokenKind::Keyword(Into))?.is_some() {
+                    break;
+                }
+                self.consume_separator(Separator::Comma)?;
             }
-            self.consume_separator(Separator::Comma)?;
+        } else {
+            let mut nodes = Vec::new();
+            loop {
+                let ast = self.parse_node(Precedence::None)?;
+                nodes.push(ast);
+                if self.consume_if(TokenKind::Keyword(Into))?.is_some() {
+                    break;
+                }
+                self.consume_separator(Separator::Comma)?;
+            }
+            rows.push(AstTuple { token: nodes[0].token().clone(), nodes });
         }
 
         let schema = self.parse_identifier()?;
@@ -82,9 +96,58 @@ mod tests {
     }
 
     #[test]
+    fn test_without_paren() {
+        let tokens = lex(r#"
+        insert
+            1, 'Alice', true
+        into test.users(id, name, is_premium)
+    "#)
+        .unwrap();
+        let mut parser = Parser::new(tokens);
+        let mut result = parser.parse().unwrap();
+        assert_eq!(result.len(), 1);
+
+        let result = result.pop().unwrap();
+        let insert = result.as_insert();
+
+        match insert {
+            AstInsert { schema, store, columns, rows, .. } => {
+                assert_eq!(schema.value(), "test");
+                assert_eq!(store.value(), "users");
+
+                assert_eq!(columns.len(), 3);
+                assert_eq!(columns[0].value(), "id");
+                assert_eq!(columns[1].value(), "name");
+                assert_eq!(columns[2].value(), "is_premium");
+
+                assert_eq!(rows.len(), 1);
+                let row = &rows[0];
+
+                assert_eq!(row.len(), 3);
+                {
+                    let id = row[0].as_literal_number();
+                    assert_eq!(id.value(), "1");
+                }
+                {
+                    let name = row[1].as_literal_text();
+                    assert_eq!(name.value(), "Alice");
+                }
+                {
+                    let is_premium = row[2].as_literal_boolean();
+                    assert_eq!(is_premium.value(), true);
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    #[test]
     fn test_multiple_rows() {
         let tokens = lex(r#"
-        insert (1, 'Alice', true), (2, 'Bob', false) into test.users(id, name, is_premium)
+        insert
+            (1, 'Alice', true),
+            (2, 'Bob', false)
+        into test.users(id, name, is_premium)
     "#)
         .unwrap();
         let mut parser = Parser::new(tokens);
@@ -128,7 +191,8 @@ mod tests {
     #[test]
     fn test_sub_query() {
         let tokens = lex(r#"
-        insert (select 1, 'Eve', false) into test.users (id, name, is_premium)
+        insert (select 1, 'Eve', false)
+        into test.users (id, name, is_premium)
     "#)
         .unwrap();
         let mut parser = Parser::new(tokens);
