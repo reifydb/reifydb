acadd4d6a2689d392bc92263f4b3cf809f077340
Dominique Chuo <dominique@reifydb.com>
2025-07-25 13:51:00 +0800

    [86] - better contains impl

--- crates/storage/src/sqlite/unversioned/contains.rs
+++ crates/storage/src/sqlite/unversioned/contains.rs
@@ -2,11 +2,20 @@
 // This file is licensed under the AGPL-3.0-or-later, see license.md file
 
 use crate::sqlite::Sqlite;
-use reifydb_core::interface::{UnversionedContains, UnversionedGet};
+use reifydb_core::interface::UnversionedContains;
 use reifydb_core::{EncodedKey, Error};
+use rusqlite::params;
 
 impl UnversionedContains for Sqlite {
     fn contains(&self, key: &EncodedKey) -> Result<bool, Error> {
-        self.get(key).map(|result| result.is_some())
+        let conn = self.get_conn();
+        let exists: bool = conn
+            .query_row(
+                "SELECT EXISTS(SELECT 1 FROM unversioned WHERE key = ?)",
+                params![key.to_vec()],
+                |row| row.get(0),
+            )
+            .unwrap();
+        Ok(exists)
     }
 }
--- crates/storage/src/sqlite/versioned/contains.rs
+++ crates/storage/src/sqlite/versioned/contains.rs
@@ -2,12 +2,20 @@
 // This file is licensed under the AGPL-3.0-or-later, see license.md file.
 
 use crate::sqlite::Sqlite;
-use reifydb_core::interface::{VersionedContains, VersionedGet};
+use reifydb_core::interface::VersionedContains;
 use reifydb_core::{EncodedKey, Version};
+use rusqlite::params;
 
 impl VersionedContains for Sqlite {
     fn contains(&self, key: &EncodedKey, version: Version) -> bool {
-        // FIXME this can be done better than this
-        self.get(key, version).is_some()
+        let conn = self.get_conn();
+        let exists: bool = conn
+            .query_row(
+                "SELECT EXISTS(SELECT 1 FROM versioned WHERE key = ? AND version <= ?)",
+                params![key.to_vec(), version],
+                |row| row.get(0),
+            )
+            .unwrap();
+        exists
     }
 }
