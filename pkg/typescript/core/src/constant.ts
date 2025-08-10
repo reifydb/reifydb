322d668999771ed1748738433463d456ce68a4b8
Dominique Chuo <dominique@reifydb.com>
2025-08-10 10:14:40 +0800

    [119] - export all value classes

--- pkg/typescript/core/src/constant.ts
+++ pkg/typescript/core/src/constant.ts
@@ -1,1 +1,7 @@
+/**
+ * MIT License
+ * Copyright (c) 2025 ReifyDB
+ * See license.md file for full license text
+ */
+
 export const UNDEFINED_VALUE = "⟪undefined⟫";
--- pkg/typescript/core/src/index.ts
+++ pkg/typescript/core/src/index.ts
@@ -3,3 +3,25 @@
  * Copyright (c) 2025 ReifyDB
  * See license.md file for full license text
  */
+
+export {Type, Value} from './value/type';
+export {BlobValue} from './value/blob';
+export {BoolValue} from './value/bool';
+export {DateValue} from './value/date';
+export {DateTimeValue} from './value/datetime';
+export {Float4Value} from './value/float4';
+export {Float8Value} from './value/float8';
+export {Int1Value} from './value/int1';
+export {Int2Value} from './value/int2';
+export {Int4Value} from './value/int4';
+export {Int8Value} from './value/int8';
+export {Int16Value} from './value/int16';
+export {IntervalValue} from './value/interval';
+export {TimeValue} from './value/time';
+export {Uint1Value} from './value/uint1';
+export {Uint2Value} from './value/uint2';
+export {Uint4Value} from './value/uint4';
+export {Uint8Value} from './value/uint8';
+export {Uint16Value} from './value/uint16';
+export {RowIdValue} from './value/rowid';
+export {Utf8Value} from './value/utf8';
