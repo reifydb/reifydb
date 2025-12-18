278bd29b5fae6b7eb057905e0b435329d88608bd
Dominique Chuo <dominique@reifydb.com>
2025-12-18 22:48:45 +0800

    add benches crate to run benchmarks

---
+++ pkg/rust/benches/src/lib.rs
@@ -0,0 +1,6 @@
+// Copyright (c) reifydb.com 2025
+// This file is licensed under the AGPL-3.0-or-later, see license.md file
+
+//! Benchmark utilities for ReifyDB TransactionStore.
+//!
+//! Run benchmarks with: `cargo bench -p reifydb-benches`
