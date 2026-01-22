5d219e6aff892b1f3b3154bcccbee30f7898963b
Dominique Chuo <dominique@reifydb.com>
2026-01-22 21:33:29 +0800

    admin: update dependencies and removes tanstack dev tools

--- crates/sub-server-admin/webapp/src/app.tsx
+++ crates/sub-server-admin/webapp/src/app.tsx
@@ -1,6 +1,5 @@
 import { RouterProvider } from '@tanstack/react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { getConnection } from '@reifydb/react'
 import { router } from './router'
 import { REIFYDB_CONFIG } from './config'
@@ -26,7 +25,6 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 }
