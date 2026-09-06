8092b053e29f0e84792e21d22996a3594d544c13
Dominique Chuo <dominique@reifydb.com>
2026-09-06 10:14:58 +0800

    group the operator state range span by batch size

--- crates/profiler/src/spec.rs
+++ crates/profiler/src/spec.rs
@@ -65,6 +65,16 @@ static SPECS: &[SpanSpec] = &[
 		render: Some(render_state_range),
 	},
 	SpanSpec {
+		name: "store::operator::range_batch",
+		duration_override: None,
+		dims: &[DimSource::Number {
+			field: "batch_size",
+			prefix: "batch",
+		}],
+		extras: &[],
+		render: None,
+	},
+	SpanSpec {
 		name: "flow::state::range",
 		duration_override: None,
 		dims: &[
